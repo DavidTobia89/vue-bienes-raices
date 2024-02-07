40ec19a409440939bf12f595cdc614ae302b399a
DavidTobia89 <dtobia@tragsa.es>
2024-02-07 13:22:57 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <v-card
+    elevation="3"
+    max-width="1200"
+    class="mx-auto" 
+  >
+    <v-layout>
+      <v-app-bar
+        color="blue-darken-1"
+      >
+        <h1>Hola</h1>
+      </v-app-bar>
+    </v-layout>
+    
+  </v-card>
+</template>
