b2055c0ee32c5c59bc618444fb7e7c63f27e331c
DavidTobia89 <dtobia@tragsa.es>
2024-02-15 11:50:59 +0100

    se crea un nuevo formulario

--- src/views/admin/NuevaPropiedadView.vue
+++ src/views/admin/NuevaPropiedadView.vue
@@ -1,7 +1,130 @@
 <script setup>
+import { useForm, useField } from 'vee-validate'
+import { validationSchema, imageSchema } from '@/validation/propiedadSchema.js'
 
+const items = [1, 2, 3, 4, 5]
+
+const { handleSubmit } = useForm({
+    validationSchema:{
+        ...validationSchema,
+        ...imageSchema
+    } 
+})
+
+const titulo = useField('titulo')
+const imagen = useField('imagen')
+const precio = useField('precio')
+const habitaciones = useField('habitaciones')
+const wc= useField('wc')
+const estacionamiento = useField('estacionamiento')
+const descripcion = useField('descripcion')
+
+
+const submit = handleSubmit((values)=>{
+  console.log(values)
+})
 </script>
 
 <template>
-    <h2 class="text-center text-h3 my-5 font-weight-bold"> Nueva Propiedad</h2>
+   
+    <v-card
+    flat
+    max-width="800"
+    class="mx-auto my-10"
+   >
+    <v-card-title
+        class="text-h4 font-weight-bold"
+        tag="h3"
+    >
+        Nueva Propiedad
+    </v-card-title>
+    <v-card-subtitle
+        class="text-h5 py-5"
+    >
+       Crea una nueva propiedad llenando el siguiente formulario
+    </v-card-subtitle>
+   
+   
+    <v-form class="mt-10">
+        <v-text-field
+            type="text"
+            label="Título Propiedad"
+            class="mb-5"
+            bg-color="blue-grey-lighten-5"
+            v-model="titulo.value.value"
+            :error-messages="titulo.errorMessage.value"
+        />
+        <v-file-input
+            accept="image/jpeg"
+            label="Fotografía"
+            prepend-icon="mdi-camera"
+            class="mb-5"
+            bg-color="blue-grey-lighten-5"
+            v-model="imagen.value.value"
+            :error-messages="imagen.errorMessage.value"
+        />
+        <v-text-field
+            type="text"
+            label="Precio"
+            class="mb-5"
+            bg-color="blue-grey-lighten-5"
+            v-model="precio.value.value"
+            :error-messages="precio.errorMessage.value"
+           
+        />
+        <v-row>
+            <v-col
+                cols="12"
+                md="4"
+            >
+                <v-select
+                    label="Habitaciones"
+                    class="mb-5"
+                    :items="items"
+                    v-model="habitaciones.value.value"
+                    :error-messages="habitaciones.errorMessage.value"
+                />
+            </v-col>
+            <v-col 
+                cols="12"
+                md="4"
+            >
+                <v-select
+                    label="Wc"
+                    class="mb-5"
+                    :items="items"
+                    v-model="wc.value.value"
+                    :error-messages="wc.errorMessage.value"
+                />
+            </v-col>
+            <v-col
+                cols="12"
+                md="4"
+            >
+                <v-select
+                    label="Lugares de Estacionamiento"
+                    class="mb-5"
+                    :items="items"
+                    v-model="estacionamiento.value.value"
+                    :error-messages="estacionamiento.errorMessage.value"
+                />
+            </v-col>
+        </v-row>
+
+       <v-textarea 
+            class="mb-5" 
+            label="Descripción"
+            v-model="titulo.value.value"
+            :error-messages="titulo.errorMessage.value"></v-textarea>
+
+       <v-checkbox label="Alberca"></v-checkbox>
+        <v-btn
+            block
+            color="pink-accent-3"
+            @click="submit"
+          >
+            Agregar Propiedad
+          </v-btn>
+    </v-form>
+  </v-card>
 </template>
